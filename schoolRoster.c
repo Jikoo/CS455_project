38e32c230907aa81e7bc8829b0454e5fe523a25e
Alex Reyes <arceldajactta@gmail.com>
2025-04-15 20:54:19 +0000

    Start

---
+++ schoolRoster.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef enum {
+    TEACHER,
+    STAFF,
+    STUDENT
+} Role;
+
+typedef struct {
+    char street[50];
+    char city[50];
+    char state[3]; // 2-Letter state code and null terminator
+    char zip[10];
+} Address;
+
+typedef struct {
+    char first_name[25];
+    char middle_name[25];
+    char last_name[25];
+} Name;
+
+struct person {
+    Name name;
+    int age;
+    Role role;
+    Address address;
+};
+
+
+
